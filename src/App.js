49cd45abe723b5d5a4d00b9a2db36678ab78de0e
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-01 08:16:16 -0500

    added sass styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
 
-export default function App() {
-    return (
-        <div>
-            hey from app
-        </div>
-    )
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      {/* Variable Example */}
+      <h1>This is a variable example</h1>
+     
+     {/* Nested Rules */}
+     <div id="nested">
+        <h3>This is a nested rules example</h3>
+     </div>
+
+     {/* Interpolation Example */}
+       <p className="interpolation">Interpolation Example.</p>
+
+       {/* Function Example */}
+       <div id="function">
+       <h3>This is a function example</h3>
+       </div>
+
+       {/* Mixin Example */}
+      <div id="mixin">
+      <p>This is mixin Example</p>
+      </div>
+    </div>
+  );
 }
+
+export default App;
