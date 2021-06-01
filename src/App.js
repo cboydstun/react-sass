ff61a707a2cac2538a3979e63f753d5a0378be46
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-01 08:34:46 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
+export default function App() {
   return (
     <div>
       {/* Variable Example */}
       <h1>This is a variable example</h1>
      
      {/* Nested Rules */}
-     <div id="nested">
+     <div className="nested">
         <h3>This is a nested rules example</h3>
      </div>
+
+      {/* Mixin Example */}
+      <div className="mixin">
+      <p>This is mixin Example</p>
+      </div>
 
      {/* Interpolation Example */}
        <p className="interpolation">Interpolation Example.</p>
 
        {/* Function Example */}
-       <div id="function">
+       <div className="function">
        <h3>This is a function example</h3>
        </div>
-
-       {/* Mixin Example */}
-      <div id="mixin">
-      <p>This is mixin Example</p>
-      </div>
     </div>
   );
 }
-
-export default App;
